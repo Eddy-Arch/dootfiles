142d5c4feebf45deabd317433a3103fab98956ea
Eddy-Arch <bishclapper69420@gmail.com>
2020-06-21 16:38:40 +0000

    dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+function fish_prompt
+    powerline-shell --shell bare $status
+end
+
+alias yourip="youtube-dl -x --audio-format mp3"
+
+#devour aliasses
+alias zathura="devour zathura"
+alias sxiv="devour sxiv"
+#alias mpv="devour mpv"
+alias gimp="devour gimp"
+alias pcmanfm="devour pcmanfm"
+#alias surf="devour tabbed surf"
+
+xset r rate 300 40
+
